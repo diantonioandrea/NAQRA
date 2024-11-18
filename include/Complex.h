6b7dbdeb72f04078b1549ce0a567ca1a93b5eecc
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-11-18 21:23:44 +0100

    Update Complex.h

--- include/Complex.h
+++ include/Complex.h
@@ -11,6 +11,9 @@
 #ifndef NAQRA_COMPLEX_H
 #define NAQRA_COMPLEX_H
 
+// Output.
+#include <stdio.h>
+
 // Neon.
 #include <arm_neon.h>
 
@@ -40,14 +43,37 @@ inline Complex C_RR_C(const Real R0, const Real R1) {
 // Complex-Complex arithmetic.
 inline Complex A_CC_C(const Complex C0, const Complex C1) { return vaddq_f64(C0, C1); }
 inline Complex S_CC_C(const Complex C0, const Complex C1) { return vsubq_f64(C0, C1); }
-inline Complex M_CC_C(const Complex C0, const Complex C1) {}
-inline Complex D_CC_C(const Complex C0, const Complex C1) {}
+
+inline Complex M_CC_C(const Complex C0, const Complex C1) {
+    Complex C2;
+
+    Real R0 = vgetq_lane_f64(C0, 0), R1 = vgetq_lane_f64(C0, 1); // C0.
+    Real R2 = vgetq_lane_f64(C1, 0), R3 = vgetq_lane_f64(C1, 1); // C1.
+
+    C2 = vsetq_lanef64(R0 * R2 - R1 * R3, C2, 0);
+    C2 = vsetq_lanef64(R0 * R3 + R1 * R2, C2, 1);
+
+    return C2;
+}
+
+inline Complex D_CC_C(const Complex C0, const Complex C1) {
+    Complex C2;
+
+    Real R0 = vgetq_lane_f64(C0, 0), R1 = vgetq_lane_f64(C0, 1); // C0.
+    Real R2 = vgetq_lane_f64(C1, 0), R3 = vgetq_lane_f64(C1, 1); // C1.
+    Real R4 = R2 * R2 + R3 * R3; // C1.
+
+    C2 = vsetq_lanef64((R0 * R2 + R1 * R3) / R4, C2, 0);
+    C2 = vsetq_lanef64((R0 * R3 - R1 * R2) / R4, C2, 1);
+
+    return C2;
+}
 
 // Complex-Real arithmetic.
 inline Complex M_CR_C(const Complex C0, const Real R0) { return vmulq_f64(C0, vdupq_n_f64(R0)); }
-inline Complex D_CR_C(const Complex C0, const Real R0) { return vdiv_f64(C0, vdupq_n_f64(R0)); }
+inline Complex D_CR_C(const Complex C0, const Real R0) { return vdivq_f64(C0, vdupq_n_f64(R0)); }
 
 // Output.
-inline void P_C_0(const Complex C0) {}
+inline void P_C_0(const Complex C0) { printf("(%.4f, %.4f)", vgetq_lane_f64(C0, 0), vgetq_lane_f64(C0, 1)); }
 
 #endif
