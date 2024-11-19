dd2742d3562371ad5fb649de14bd38f9dab98b0f
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-11-19 11:03:11 +0100

    Create Matrix.h

---
+++ include/Matrix.h
@@ -0,0 +1,25 @@
+/**
+ * @file Matrix.h
+ * @author Andrea Di Antonio (github.com/diantonioandrea)
+ * @brief Complex matrices.
+ * @date 2024-11-19
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef NAQRA_MATRIX_H
+#define NAQRA_MATRIX_H
+
+// Complex numbers.
+#include "./Complex.h"
+
+// Hessenberg form.
+
+void Hsn_CqmN_0(Complex*, const Natural);
+
+// Output.
+
+void Pn_CmNN_0(const Complex*, const Natural, const Natural);
+
+#endif
