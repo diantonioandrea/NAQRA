dd8dd453f9e19d5338dc63e2c13713d77fa10b68
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-11-19 11:03:13 +0100

    Create NAQRA_Matrix.c

---
+++ src/NAQRA_Matrix.c
@@ -0,0 +1,31 @@
+/**
+ * @file NAQRA_Matrix.c
+ * @author Andrea Di Antonio (github.com/diantonioandrea)
+ * @brief include/Matrix.h implementation.
+ * @date 2024-11-19
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include "../include/Matrix.h"
+
+// Output.
+
+/**
+ * @brief Print with new line [Pn] Complex Matrix [Cm].
+ * 
+ * @param Cm0 Complex Matrix.
+ * @param N0 Rows.
+ * @param N1 Columns.
+ */
+void Pn_CmNN_0(const Complex* Cm0, const Natural N0, const Natural N1) {
+    register Natural N2 = 0, N3;
+
+    for(; N2 < N0; ++N2) {
+        for(N3 = 0; N3 < N1 - 1; ++N3)
+            P_C_0(Cm0[N2 + N3 * N0]);
+
+        Pn_C_0(Cm0[N2 + (N1 - 1) * N0]);
+    }
+}
