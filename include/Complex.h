7e220c6d0d99005fb1bb6b17d167f2b0dbd64e9f
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-11-19 10:36:27 +0100

    Update Complex.h

--- include/Complex.h
+++ include/Complex.h
@@ -28,10 +28,12 @@ typedef float64x2_t Complex; // Complex numbers.
 
 
 // Complex "constructors".
+
 inline Complex C_R_C(const Real R0) { Complex C0 = {R0, 0.0}; return C0; }
 inline Complex C_RR_C(const Real R0, const Real R1) { Complex C0 = {R0, R1}; return C0; }
 
 // Complex-Complex arithmetic.
+
 inline Complex A_CC_C(const Complex C0, const Complex C1) { return vaddq_f64(C0, C1); }
 inline Complex S_CC_C(const Complex C0, const Complex C1) { return vsubq_f64(C0, C1); }
 
@@ -51,10 +53,12 @@ inline Complex D_CC_C(const Complex C0, const Complex C1) {
 }
 
 // Complex-Real arithmetic.
+
 inline Complex M_CR_C(const Complex C0, const Real R0) { return vmulq_f64(C0, vdupq_n_f64(R0)); }
 inline Complex D_CR_C(const Complex C0, const Real R0) { return vdivq_f64(C0, vdupq_n_f64(R0)); }
 
 // Output.
+
 inline void P_C_0(const Complex C0) { printf("(%.4f, %.4f)", vgetq_lane_f64(C0, 0), vgetq_lane_f64(C0, 1)); }
 
 #endif
