50f8ab314b166d8f82d578f9269e9cf6a6649bb1
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-11-19 15:56:07 +0100

    Create Vector.h

---
+++ include/Vector.h
@@ -0,0 +1,22 @@
+/**
+ * @file Vector.h
+ * @author Andrea Di Antonio (github.com/diantonioandrea)
+ * @brief Complex vectors.
+ * @date 2024-11-19
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef NAQRA_VECTOR_H
+#define NAQRA_VECTOR_H
+
+// Complex numbers.
+#include "./Complex.h"
+
+// Output.
+
+void Pn_CrvN_0(const Complex*, const Natural);
+void Pn_CcvN_0(const Complex*, const Natural);
+
+#endif
