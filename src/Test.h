689a2eee89e207be19cd1fa3a527bfcb5b32d043
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-11-21 18:42:38 +0100

    Create Test.h

---
+++ src/Test.h
@@ -0,0 +1,18 @@
+/**
+ * @file Test.h
+ * @author Andrea Di Antonio (github.com/diantonioandrea)
+ * @brief Testing header.
+ * @date 2024-11-21
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef NAQRA_TEST_H
+#define NAQRA_TEST_H
+
+// Headers.
+#include <stdlib.h>
+#include <time.h>
+
+#endif
